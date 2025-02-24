fde6ed0ff39e48ad7e8a451117cd800a552ebb44
Imad AYAD <cbcj6942@EB-L-OR1034591.ad-subs.w2k.francetelecom.fr>
2025-02-24 22:27:49 +0100

    feat: some modifs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Milvus-training
+
+To get started:
+
+
+```bash
+# Setup env virtual
+python3 -m venv milvus_env
+source milvus_env/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+from typing import List
 
+# Choose a model
 model_name = "sentence-transformers/all-mpnet-base-v2"
-model_kwargs = {'device': 'cpu'}
-encode_kwargs = {'normalize_embeddings': False}
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": False}
+
+# Load Embeddor engine
 hf = HuggingFaceEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs
+    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
 )
+
+# Embed sentence
+
+sentence_A_vector: List[float] = hf.embed_query("I like eating banana")
+sentence_B_vector: List[float] = hf.embed_query("Fruits are so good")
