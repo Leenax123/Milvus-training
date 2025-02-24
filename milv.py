7bb4126f38e1c73d04460ad6b553b82cc3b08ea2
Leenax123 <leena.ayad@etu.unistra.fr>
2025-02-24 21:58:54 +0100

    milv.py

--- milv.py
+++ milv.py
@@ -2,14 +2,13 @@ import requests
 from pymilvus import Collection
 
 # Connexion à Milvus
-milvus_client = Collection("votre_collection")  # Remplacez par le nom de votre collection Milvus
+milvus_client = Collection("collection") 
 
-# Exemple de recherche dans Milvus pour récupérer un vecteur
-query_result = milvus_client.query(expr="id == 1")  # Par exemple, récupérer un vecteur avec id == 1
+query_result = milvus_client.query(expr="id == 1")   
 
 vector_to_send = query_result[0]["vector"]  # Extraire le vecteur
 
-# Envoi du vecteur à FastAPI
+
 url = "http://127.0.0.1:8000/search/"
 payload = {"vector": vector_to_send}
 response = requests.post(url, json=payload)
