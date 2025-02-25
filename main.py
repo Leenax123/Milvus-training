0ebb1f3610d885eaef2c5b6bc1bd55dca71d15e4
Alix <alix-machard@outlook.fr>
2025-02-25 11:50:26 +0100

    added fastapi endpoint

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer, util
 from typing import List
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
 # Choose a model
 model_name = "sentence-transformers/all-mpnet-base-v2"
 model_kwargs = {"device": "cpu"}
@@ -12,15 +15,19 @@ hf = HuggingFaceEmbeddings(
     model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
 )
 
-# Embed sentence
 
-sentence_A_vector: List[float] = hf.embed_query("I like eating banana")
-sentence_B_vector: List[float] = hf.embed_query("Fruits are so good")
-sentence_C_vector: List[float] = hf.embed_query("Tomorrow will be raining")
+@app.get("/similarity")
+def compute_similarity(sentence_a, sentence_b):
 
-# Compute similarity
+    # Embed sentences
+    sentence_A_vector: List[float] = hf.embed_query(sentence_a)
+    sentence_B_vector: List[float] = hf.embed_query(sentence_b)
 
-similarityA_B = float(util.pytorch_cos_sim(sentence_A_vector, sentence_B_vector)) # 0.55
-similarityA_C = float(util.pytorch_cos_sim(sentence_A_vector, sentence_C_vector)) # 0.07
+    # Compute similarity
+    similarity = float(util.pytorch_cos_sim(sentence_A_vector, sentence_B_vector))
 
-# Le modele a bien repere la similarite entre A & B, par rapport a C.
+    return {"similarity": similarity}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
